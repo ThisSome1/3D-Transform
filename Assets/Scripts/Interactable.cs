0a77b57149dda6c51be59aaceb5c5554fe8ab303
ThisSome1 <H.BehrooziB4U@gmail.com>
2024-10-07 15:32:52 +0330

    set custom gravity

--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -6,14 +6,20 @@ public class Interactable : MonoBehaviour
 {
     internal bool Highlighted => highlighted;
 
+    [SerializeField] float gravity = 9.807f;
+
     bool highlighted;
+    Rigidbody rigidBody;
 
     void Start()
     {
+        rigidBody = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
+        if (!rigidBody.isKinematic)
+            rigidBody.velocity -= new Vector3(0, gravity, 0) * Time.deltaTime;
     }
 
     internal void Highlight()
